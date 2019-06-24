773978bb28b4734a42a36c9c26f856fad6ee3d9d
Tongmee Xiong <tongmeexiong@gmail.comlspwd>
2019-06-24 13:08:49 -0500

    added protect routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   HashRouter as Router,
   Route,
@@ -6,22 +6,30 @@ import {
   Switch,
 } from 'react-router-dom';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+
 import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
-import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
+import Dashboard from '../Dashboard/Dashboard'
+import CreateNewApis from '../CreateNewApis/CreateNewApis'
+import AddAircraftForm from '../AddAircraftForm/AddAircraftForm'
+import AddPassengerForm from '../AddPassengerForm/AddPassengerForm'
+import AddCrewForm from '../AddCrewForm/AddCrewForm'
+import ReviewPage from '../ReviewPage/ReviewPage'
+import CrewInfo from '../CrewInfo/CrewInfo'
+import AirCraftInfo from '../AirCraftInfo/AirCraftInfo'
+import UserInfo from '../UserInfo/UserInfo'
+import PassengerInfo from '../PassengerInfo/PassengerInfo'
+
 
 import './App.css';
-import Aircraft from '../Aircraft/Aircraft';
 
 class App extends Component {
-  componentDidMount () {
-    this.props.dispatch({type: 'FETCH_USER'})
+  componentDidMount() {
+    this.props.dispatch({ type: 'FETCH_USER' })
   }
 
   render() {
@@ -32,37 +40,91 @@ class App extends Component {
           <Switch>
             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
             <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
-            {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/home will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-            Even though it seems like they are different pages, the user is always on localhost:3000/home */}
+
+            {/*  Protected route for the Dashboard View */}
             <ProtectedRoute
               exact
               path="/home"
-              component={UserPage}
+              component={Dashboard}
             />
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
+
+            {/* Protected route for creating new APIS View  */}
             <ProtectedRoute
               exact
-              path="/info"
-              component={InfoPage}
+              path="/apis"
+              component={CreateNewApis}
             />
+
+            {/* Protected route for Review APIS View */}
+            <ProtectedRoute
+              exact
+              path="/reviewpage"
+              component={ReviewPage}
+            />
+
+            {/*  Adding new flight info starts here */}
+            {/* Protected route for adding new aircraft */}
+            <ProtectedRoute
+              exact
+              path="/addaircraft"
+              component={AddAircraftForm}
+            />
+
+            {/* Protected route for adding new crew member */}
+            <ProtectedRoute
+              exact
+              path="/addcrew"
+              component={AddCrewForm}
+            />
+
+            {/* Protected route for adding new passenger  */}
+            <ProtectedRoute
+              exact
+              path="/addpassenger"
+              component={AddPassengerForm}
+            />
+            {/*  Adding new flight info ends here */}
+
+            {/*  User, Crew, Aircraft & Passenger information view from Nav Starts Here */}
+            {/*  Protected route to see User Information View from Nav */}
+            <ProtectedRoute
+              exact
+              path="/userinfo"
+              component={UserInfo}
+            />
+
+            {/*  Protected route to see Passenger Information View from Nav */}
+            <ProtectedRoute
+              exact
+              path="/passengerinfo"
+              component={PassengerInfo}
+            />
+
+            {/*  Protected route to see Crew Member Information View from Nav */}
+            <ProtectedRoute
+              exact
+              path="/crewinfo"
+              component={CrewInfo}
+            />
+
+            {/*  Protected route to see Aircraft Details Information View from Nav */}
+            <ProtectedRoute
+              exact
+              path="/aircraftinfo"
+              component={AirCraftInfo}
+            />
+            {/*  User, Crew & Passenger information from Nav Ends Here */}
+
+
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
           </Switch>
           <Footer />
         </div>
-        <Aircraft />
       </Router>
-  )}
+    )
+  }
 }
 
 export default connect()(App);
+
