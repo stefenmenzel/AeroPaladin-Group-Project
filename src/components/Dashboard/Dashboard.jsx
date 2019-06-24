0a12ea251d98556fe117af9b807f1ddce7e98cb7
heensaleem <heenakouser440@gmail.com>
2019-06-24 16:18:28 -0500

    added dashboard table

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,89 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react'
 
-class Dashboard extends Component{
+class Dashboard extends Component {
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                testing dashboard
+                <div>
+                   <h2>Welcome Back Tom</h2>
+                </div>
+                <p>Active APIS Trips</p>
+                <table class="ui celled table">
+                    <thead class="">
+                        <tr class="">
+                            <th class="">From</th>
+                            <th class="">To</th>
+                            <th class="">Date</th>
+                            <th class="">Edit/Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        <tr class="">
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                        </tr>
+                        <tr class="">
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <p>Upcoming APIS Trips</p>
+                <table class="ui celled table">
+                    <thead class="">
+                        <tr class="">
+                            <th class="">From</th>
+                            <th class="">To</th>
+                            <th class="">Date</th>
+                            <th class="">Edit/Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        <tr class="">
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                        </tr>
+                        <tr class="">
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table class="ui celled table">
+                    <thead class="">
+                        <tr class="">
+                            <th class="">From</th>
+                            <th class="">To</th>
+                            <th class="">Date</th>
+                            <th class="">Edit/Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        <tr class="">
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                        </tr>
+                        <tr class="">
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class="">Cell</td>
+                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                        </tr>
+                    </tbody>
+
+                </table>
             </div>
         )
     }
