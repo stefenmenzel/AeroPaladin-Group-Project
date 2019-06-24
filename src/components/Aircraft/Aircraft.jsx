b9d9cfeccbfd1c12999100d3d946ccd476681205
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-06-24 12:02:13 -0500

    added some stuff to the aircraft inputs

--- src/components/Aircraft/Aircraft.jsx
+++ src/components/Aircraft/Aircraft.jsx
@@ -1,18 +1,63 @@
 import React,{Component} from 'react';
-import {Input} from 'semantic-ui-react';
+import {Input, Label} from 'semantic-ui-react';
 
 class Aircraft extends Component{
+
+    state = {
+        tailNumber: '',
+        aircraftType: '',
+        color: '',
+        callSign: '',
+        decalNumber: ''
+    }
 
     render(){
         return(
             <>
-                <Input
-                    icon="tags"
-                    iconPosition='left'
-                    label={{tag: true, content: 'tail number'}}
-                    labelPosition="right"
-                    placeholder="Tail Number"
-                />             
+                <Label>
+                    <Input                                                
+                        placeholder="Tail Number"
+                    />
+                    <span style={{marginLeft: "10px"}}>  
+                        Tail Number
+                    </span>
+                </Label>
+
+                <Label>
+                    <Input                        
+                        placeholder="Aircraft Type"
+                    />
+                    <span style={{ marginLeft: "10px" }}>
+                        Aircraft Type
+                    </span>
+                </Label>
+
+                <Label>
+                    <Input                        
+                        placeholder="Color"
+                    />
+                    <span style={{ marginLeft: "10px" }}>
+                        Color
+                    </span>
+                </Label>
+
+                <Label>
+                    <Input                        
+                        placeholder="Call Sign"
+                    />
+                    <span style={{ marginLeft: "10px" }}>
+                        Call Sign
+                    </span>
+                </Label>
+
+                <Label>
+                    <Input                        
+                        placeholder="CBP Decal Number"
+                    />
+                    <span style={{ marginLeft: "10px" }}>
+                        CBP Decal Number
+                    </span>
+                </Label>
             </>
         )
     }
