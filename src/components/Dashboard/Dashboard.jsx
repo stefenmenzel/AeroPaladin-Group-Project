af42e8a804deefa69fd23cd199cc23734bb97fba
heensaleem <heenakouser440@gmail.com>
2019-06-25 10:01:41 -0500

    sagas and reducers data showing up on DOM

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { connect } from 'react-redux';
+import { Icon, Label, Menu, Table } from 'semantic-ui-react';
+import "./Dashboard.css";
 
 class Dashboard extends Component {
-
+   componentDidMount() {
+       this.props.dispatch({ type:'FETCH_APIS_TRIPS' })
+   }
     render() {
         return (
             <div>
+                <pre>{JSON.stringify(this.props.apisTrips)}</pre>
                 <div>
                    <h2>Welcome Back Tom</h2>
                 </div>
-                <p>Active APIS Trips</p>
+                <p> APIS Trips</p>
                 <table class="ui celled table">
                     <thead class="">
                         <tr class="">
@@ -21,72 +26,28 @@ class Dashboard extends Component {
                     </thead>
                     <tbody class="">
                         <tr class="">
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                            <td >Cell</td>
+                            <td >Cell</td>
+                            <td >Cell</td>
+                            <td ><button>Edit</button> <button>Delete</button></td>
                         </tr>
                         <tr class="">
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class=""><button>Edit</button> <button>Delete</button></td>
+                            <td >Cell</td>
+                            <td >Cell</td>
+                            <td >Cell</td>
+                            <td ><button>Edit</button> <button>Delete</button></td>
                         </tr>
                     </tbody>
                 </table>
-                <p>Upcoming APIS Trips</p>
-                <table class="ui celled table">
-                    <thead class="">
-                        <tr class="">
-                            <th class="">From</th>
-                            <th class="">To</th>
-                            <th class="">Date</th>
-                            <th class="">Edit/Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody class="">
-                        <tr class="">
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class=""><button>Edit</button> <button>Delete</button></td>
-                        </tr>
-                        <tr class="">
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class=""><button>Edit</button> <button>Delete</button></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <table class="ui celled table">
-                    <thead class="">
-                        <tr class="">
-                            <th class="">From</th>
-                            <th class="">To</th>
-                            <th class="">Date</th>
-                            <th class="">Edit/Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody class="">
-                        <tr class="">
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class=""><button>Edit</button> <button>Delete</button></td>
-                        </tr>
-                        <tr class="">
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class="">Cell</td>
-                            <td class=""><button>Edit</button> <button>Delete</button></td>
-                        </tr>
-                    </tbody>
-
-                </table>
+                
             </div>
         )
     }
 }
 
-export default Dashboard
+
+const mapStateToProps = state => ({
+    apisTrips : state.dashboardReducer
+})
+
+export default connect(mapStateToProps)(Dashboard);
