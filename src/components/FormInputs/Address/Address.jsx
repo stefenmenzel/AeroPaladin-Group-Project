59ffaa138aff8b313d812776404f5eeb2e59d6d8
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-06-25 16:17:20 -0500

    finally got the aircraft, owner, operator to POST, I had to post to four separate tables and use Async/Await for the first time

--- src/components/FormInputs/Address/Address.jsx
+++ src/components/FormInputs/Address/Address.jsx
@@ -3,6 +3,23 @@ import {Input, Label} from 'semantic-ui-react';
 
 import '../FormInputs.css';
 
+/**
+ * This component will throw a few input fields on the DOM.
+ *
+ * This component is useless without a form.
+ *
+ * You must send props into this component in order for it to
+ * work properly.
+ * 
+ * If you'd like for this component to ask for a country code
+ * you must pass in a prop called "countryCode" and set it to true
+ *
+ * You must send in a "handleChange" function,
+ * and a "stateType" prop. The handleChange function
+ * will send the value of the input field back to the parent component form
+ * and the state type will correspond to an object to create/add values to
+ * in the parent components state (aircraft owner operator)
+ */
 class Address extends Component{
 
     conditionalCountryCode = () => {
