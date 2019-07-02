dbce608e2a08e06b515d0d0338143a3281497243
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-07-02 09:47:51 -0500

    got the crew update form setup...working on getting to it from the dom

--- src/components/CrewInfo/CrewUpdateForm.jsx
+++ src/components/CrewInfo/CrewUpdateForm.jsx
@@ -26,10 +26,60 @@ class CrewUpdateForm extends Component{
         this.props.history.push('/crewinfo');
     }
 
+    handleChange = (propertyToChange, newProperty, event) => {
+        this.setState({
+            ...this.state,
+            [propertyToChange]: {
+                ...this.state[propertyToChange],
+                [newProperty]: event.target.value
+            }
+        })
+
+    }
+
     render() {
         return(
             <div>
                 <h1>Edit Passenger</h1>
+                <form className="addForm" onSubmit={this.handleSubmit}>
+                    <h2>Crew Info</h2>
+                    <Name extended={true} handleChange={this.handleChange} stateType="crew" />
+                    <Divider />
+                    <Address handleChange={this.handleChange} stateType="crew" />
+                    <Divider />
+                    <Contact handleChange={this.handleChange} stateType="crew" />
+                    <Divider />
+                    <h2>Travel Document 1</h2>
+                    <TravelDocuments handleChange={this.handleChange} stateType="travelDocumentOne" />
+                    <h2>Travel Document 2</h2>
+                    <TravelDocuments handleChange={this.handleChange} stateType="travelDocumentTwo" />
+
+                    <div className="formButtons">
+                        <Grid columns='equal'>
+                            <Grid.Column width={10}></Grid.Column>
+                            <Grid.Column width={3}>
+                                <Button
+                                    type="button"
+                                    onClick={this.handleCancel}
+                                    secondary
+                                    className="formButton"
+                                >
+                                    Cancel
+                                </Button>
+                            </Grid.Column>
+
+                            <Grid.Column width={3}>
+                                <Button
+                                    type="submit"
+                                    primary
+                                    className="formButton"
+                                >
+                                    Add Crew
+                                </Button>
+                            </Grid.Column>
+                        </Grid>
+                    </div>
+                </form>
             </div>
         )
     }
