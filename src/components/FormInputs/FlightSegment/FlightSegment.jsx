ff9f16fa04485e2d4f51387ec3e9c66db97de97c
heensaleem <heenakouser440@gmail.com>
2019-07-02 11:11:27 -0500

    added timestamp

--- src/components/FormInputs/FlightSegment/FlightSegment.jsx
+++ src/components/FormInputs/FlightSegment/FlightSegment.jsx
@@ -1,23 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {Input, Label, Select, Checkbox} from 'semantic-ui-react';
-import {Divider, Button, Grid} from 'semantic-ui-react';
-import {DateInput} from 'semantic-ui-calendar-react';
+import { Input, Label, Select, Checkbox } from 'semantic-ui-react';
+import { Divider, Button, Grid } from 'semantic-ui-react';
+import { DateInput, TimeInput } from 'semantic-ui-calendar-react';
 import '../FormInputs.css';
 
 class FlightSegment extends Component {
 
     state = {
-        date: '',
-        airport: '',
-        estimatedTime: '',
-        timeZone: ''
+        departure : {
+         
+        },
+        arrival : {
+
+        },
+          date: '',
+          time: ''
+        // airport: '',
+        // estimatedTime: '',
+        // timeZone: ''
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
         console.log("doing a submit", this.state);
-        this.props.dispatch({ type: 'SET_APIS_FLIGHTSEGMENTONE', payload: this.state})
+        this.props.dispatch({ type: 'SET_APIS_FLIGHTSEGMENTONE', payload: this.state })
         this.props.nextStep();
     }
 
@@ -27,146 +34,170 @@ class FlightSegment extends Component {
     }
 
     // handles on inputs on form and sets state
-  handleChange = property => event => {
-    this.setState({
-      ...this.state,
-      [property]: event.target.value
-    });
-  };
+    handleDepartureChange = property => event => {
+        this.setState({
+            departure:{
+            ...this.state.departure,
+            [property]: event.target.value
+            }
+        });
+    };
 
-    onDateChange = (event, {name, value}) => {
+    handleArrivalChange = property => event => {
+        this.setState({
+            arrival:{
+            ...this.state.departure,
+            [property]: event.target.value
+            }
+        });
+    };
+
+    onChange = (event, { name, value }) => {
         //nothing to see here. move along
         this.setState({
             ...this.state,
-            date:value
+            [name]: value
         })
     }
+    
 
-    render () {
+    render() {
         return (
             <div className="formInputs">
-                 <form className="addForm" onSubmit={this.handleSubmit}>
-                <h2>Flight Segment One</h2>
-                <p>Departure</p>
-            <Label className="formInputLabel">            
-                <Input className="formInput"
-                    onChange={this.handleChange("airport")}
-                    placeholder="Airport"
-                />
-                <span>
-                   Airport
-                </span>
-            </Label>
+                <form className="addForm" onSubmit={this.handleSubmit}>
+                    <h2>Flight Segment One</h2>
+                    <p>Departure</p>
+                    <Label className="formInputLabel">
+                        <Input className="formInput"
+                            onChange={this.handleDepartureChange("airportcode")}
+                            placeholder="Airport"
+                        />
+                        <span>
+                            Airport
+                       </span>
+                    </Label>
 
-            <Label className="formInputLabel">
-                <Input className="formInput"
-                    onChange={this.handleChange("estimatedTime")}
-                    placeholder="Estimated Time of Departure"
-                />
-                <span>
-                    Estimated Time
-                </span>
-            </Label>
-            <Label className="formInputLabel">
-                <Input className="formInput"
-                    onChange={this.handleChange("timeZone")}
-                    placeholder="Time Zone"
-                />
-                <span>
-                    Time Zone
-                </span>
-            </Label>
-            <Label className="formInputLabel">
-                    <DateInput className="formAltInput"
-                        name="departureDate"
-                        value={this.state.date}
-                        placeholder = "Estimated Departure Date"
-                        iconPosition="left"                        
-                        onChange={this.onDateChange}
-                        style={{width:'100%'}}
-                        dateFormat="YYYY-MM-DD"
-                    />
-                    <span>
-                        Estimated Departure Date
+                    <Label className="formInputLabel">
+                        <TimeInput className="formAltInput"
+                            name="time"
+                            value={this.state.time}
+                            iconPosition="left"
+                            onChange={this.handleDepartureChange("airportcode")}
+                            placeholder="Estimated Time of Departure"
+                            style={{ width: '100%' }}
+                        />
+                        <span>
+                            Estimated Time
+                       </span>
+                    </Label>
+                    <Label className="formInputLabel">
+                    <Input className="formInput"
+                            onChange={this.handleDepartureChange("time_zone")}
+                            placeholder="Time Zone"
+                        />
+                        <span>
+                            Time Zone
+                        </span>
+                    </Label>
+                    <Label className="formInputLabel">
+                        <DateInput className="formAltInput"
+                            name="date"
+                            value={this.state.date}
+                            placeholder="Estimated Departure Date"
+                            iconPosition="left"
+                            onChange={this.handleDepartureChange("departure_date")}
+                            style={{ width: '100%' }}
+                            dateFormat="YYYY-MM-DD"
+                        />
+                        <span>
+                            Estimated Departure Date
                     </span>
-                </Label>
-            <Divider />
-            <p>Arrival</p>
-            <Label className="formInputLabel">            
-                <Input className="formInput"
-                    onChange={this.handleChange("airport")}
-                    placeholder="Airport"
-                />
-                <span>
-                   Airport
+                    </Label>
+                    <Divider />
+                    <p>Arrival</p>
+                    <Label className="formInputLabel">
+                        <Input className="formInput"
+                            onChange={this.handleArrivalChange("airport")}
+                            placeholder="Airport"
+                        />
+                        <span>
+                            Airport
                 </span>
-            </Label>
+                    </Label>
 
-            <Label className="formInputLabel">
-                <Input className="formInput"
-                    onChange={this.handleChange("estimatedTime")}
-                    placeholder="Estimated Time of Departure"
-                />
-                <span>
-                    Estimated Time
+                    <Label className="formInputLabel">
+                    <TimeInput className="formAltInput"
+                            name="time"
+                            value={this.state.time}
+                            iconPosition="left"
+                            onChange={this.onChange}
+                            placeholder="Estimated Time of Departure"
+                            style={{ width: '100%' }}
+                        />
+                        <span>
+                            Estimated Time
                 </span>
-            </Label>
-            <Label className="formInputLabel">
-                <Input className="formInput"
-                    onChange={this.handleChange("timeZone")}
-                    placeholder="Time Zone"
-                />
-                <span>
-                    Time Zone
-                </span>
-            </Label>
+                    </Label>
+                    <Label className="formInputLabel">
+                        <Input className="formInput"
+                            onChange={this.handleArrivalChange("time_zone")}
+                            placeholder="Time Zone"
+                        />
+                        <span>
+                            Time Zone
+                        </span>
+                    </Label>
 
-            <Label className="formInputLabel">
-                    <DateInput className="formAltInput"
-                        name="departureDate"
-                        value={this.state.date}
-                        placeholder = "Estimated Departure Date"
-                        iconPosition="left"                        
-                        onChange={this.onDateChange}
-                        style={{width:'100%'}}
-                        dateFormat="YYYY-MM-DD"
-                    />
-                    <span>
-                        Estimated Departure Date
+                    <Label className="formInputLabel">
+                        <DateInput className="formAltInput"
+                            name="departureDate"
+                            value={this.state.date}
+                            placeholder="Estimated Departure Date"
+                            iconPosition="left"
+                            onChange={this.onChange}
+                            style={{ width: '100%' }}
+                            dateFormat="YYYY-MM-DD"
+                        />
+                        <span>
+                            Estimated Departure Date
                     </span>
-                </Label>
+                    </Label>
 
-            
-       
-        <div className="formButtons">
-        <Grid columns='equal'>
-            <Grid.Column width={10}></Grid.Column>
-            <Grid.Column width={3}>
-                <Button 
-                    type="button"
-                    onClick={this.handlePrevious}
-                    secondary 
-                    className="formButton"
-                >
-                   Back
-                </Button>
-            </Grid.Column>
+                    <div className="formButtons">
+                        <Grid columns='equal'>
+                            <Grid.Column width={10}></Grid.Column>
+                            <Grid.Column width={3}>
+                                <Button
+                                    type="button"
+                                    onClick={this.handlePrevious}
+                                    secondary
+                                    className="formButton"
+                                >
+                                    Back
+                                </Button>
+                            </Grid.Column>
 
-            <Grid.Column width={3}>
-                <Button
-                    type="submit"
-                    primary 
-                    className="formButton"
-                >
-                    NEXT
-                </Button>
-            </Grid.Column>
-        </Grid>
-        </div>                    
-        </form>
-    </div>            
-    )
-}
+                            <Grid.Column width={3}>
+                                <Button
+                                    type="submit"
+                                    primary
+                                    className="formButton"
+                                >
+                                    NEXT
+                            </Button>
+                            </Grid.Column>
+                        </Grid>
+                    </div>
+                </form>
+            </div>
+        )
+    }
 }
 
-export default connect ()(FlightSegment);
+const mapStateToProps = (reduxState) => {
+    return {
+
+    }
+}
+
+export default connect(mapStateToProps)(FlightSegment);
