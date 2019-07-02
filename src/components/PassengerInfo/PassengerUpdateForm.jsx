cc1289dfd1b75b9a9e5222f9ef546bd2222b46d6
Stefen Menzel <stuff@Stefens-MacBook-Pro.local>
2019-07-02 09:37:04 -0500

    made a blank passenger update form

---
+++ src/components/PassengerInfo/PassengerUpdateForm.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { Divider, Button, Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+
+import Name from '../FormInputs/Name/Name.jsx';
+import Address from '../FormInputs/Address/Address.jsx';
+import Contact from '../FormInputs/Contact/Contact.jsx';
+import TravelDocuments from '../FormInputs/TravelDocuments/TravelDocuments.jsx';
+
+class PassengerUpdateForm extends Component {
+    state = {
+
+    }
+
+    componentDidMount() {
+        //fetch info here
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log('doing a submitto');
+    }
+
+    handleCancel = () => {
+        this.props.history.push('/crewinfo');
+    }
+
+    render() {
+        return (
+            <div>Testing form</div>
+        )
+    }
+}
+
+const mapStateToProps = (reduxState) => {
+    return {
+        reduxState
+    }
+}
+
+export default connect(mapStateToProps)(PassengerUpdateForm);
