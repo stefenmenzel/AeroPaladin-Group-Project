9db66dba304d5aa9197616ddc5f6cbfeed7ed676
Madeline Morton <madelinemorton614@gmail.com>
2019-07-05 10:51:44 -0500

    post stuff

--- server/routes/apis.router.js
+++ server/routes/apis.router.js
@@ -9,22 +9,26 @@ const router = express.Router();
 /*** POST apis route ***/
 router.post('/', rejectUnauthenticated, async (req, res) => {
     console.log('req.body here: ', req.body);
-    
-    const connection = await pool.connect();
 
     //returning data from DB queriess
     let airport_id_One = 0;
+    let airport_id_Two = 0;
+    let airport_id_Three = 0;
+    let airport_id_Four = 0;
     let itinerary_id_One = 0;
+    let itinerary_id_Two = 0;
     let flight_id_One = 0;
-    let emergency_Contact_id = 0;
+    let flight_id_Two = 0;
+
     //
     let crewID = 0;
-    
+    let result;
+
     //objects
     let crewData = req.body.crew;
     let aircraftData = req.body.aircraft;
     let flightSegmentOneData = req.body.flightSegmentOne;
-    let flightSegmentTwoData = req.body.flightSegmentTwo;
+    let flightSegmentTwoData = req.body.flightSegmentTwo; 
 
     //array of objects
     let paxData = req.body.passenger;
@@ -63,28 +67,91 @@ router.post('/', rejectUnauthenticated, async (req, res) => {
                               "flight_id")
                               VALUES ($1, $2); `
 
-    // try {
-    //     await connection.query('BEGIN');
+    const connection = await pool.connect();
+    
+    try {
+        await connection.query('BEGIN');
         
+        let departure = flightSegmentOneData.departure;
+        let arrival = flightSegmentOneData.arrival;
+
+        //AIRPORT POST
+        result = await connection.query(airportPost, 
+            [departure.airport, 
+            departure.city, 
+            departure.country, 
+            departure.state,
+            departure.description])
+            airport_id_One = result.rows[0].id;
+
+        result = await connection.query(airportPost, 
+            [
+              arrival.airport,
+              arrival.city,
+              arrival.country,
+              arrival.state,
+              arrival.description
+            ])
+            airport_id_Two = result.rows[0].id;
+
+        departure = flightSegmentTwoData.departure;
+        arrival = flightSegmentTwoData.arrival;
         
-    //     //passenger post loop
-    //     for (let i=0; i <= paxData.length; i++){
+        result = await connection.query(airportPost, 
+            [departure.airport, 
+            departure.city, 
+            departure.country, 
+            departure.state,
+            departure.description])
+            airport_id_Three = result.rows[0].id;
 
-    //     }
+        result = await connection.query(airportPost, 
+            [
+              arrival.airport,
+              arrival.city,
+              arrival.country,
+              arrival.state,
+              arrival.description
+            ])
+            airport_id_Four = result.rows[0].id;
 
+    // ITINERARY POST
+        result = await connection.query(itineraryPost, 
+                [
+                    airport_id_One, 
+                    flightSegmentOne.departure.date + flightSegmentOne.departure.time,
+                    airport_id_Two,
+                    flightSegmentOne.arrival.date + flightSegmentOne.arrival.time
+                ])
+                itinerary_id_One = result.rows[0].id;
+                
+        result = await connection.query(itineraryPost, 
+                [
+                    airport_id_Three, 
+                    departure.date + departure.time,
+                    airport_id_Four,
+                    arrival.date + arrival.time
+                ])
+                itinerary_id_Two = result.rows[0].id;
 
+        // FLIGHT POST
+        result = await connection.query(flightPost,
+            [
+                itinerary_id_One,
+                crew
+            ])
 
-    //     //committing all posts
-    //     await connection.query('COMMIT');
-    //     console.log('made it through');
-    //     res.sendStatus(201);
-    // }
-    // catch(error){
-    //     await connection.query('ROLLBACK');
-    //     res.sendStatus(500);
-    // }
-    // finally{
-    //     connection.release();
-    // }
+        //committing all posts
+        await connection.query('COMMIT');
+        console.log('made it through');
+        res.sendStatus(201);
+    }
+    catch(error){
+        await connection.query('ROLLBACK');
+        res.sendStatus(500);
+    }
+    finally{
+        connection.release();
+    }
 
 module.exports = router;
